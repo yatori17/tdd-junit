d017081ba5773d2e0cce543be104e15c149bc5eb
Leonardo Andrade <ows.silva@loginlogistica.com.br>
2022-06-06 20:14:22 -0300

    Initial commit

---
+++ src/main/java/tddjunit/TddJunitApplication.java
@@ -0,0 +1,13 @@
+package tddjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tddjunit/TddJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package tddjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
